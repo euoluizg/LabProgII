4284fea5dba4307b30b95cca9d44160ea64f364c
euoluizg <gmelo17177@gmail.com>
2025-08-29 15:29:36 -0300

    fix

--- listaExecAV1-02.py
+++ listaExecAV1-02.py
@@ -45,6 +45,8 @@ print("-----------------------------------")
 print("")
 print("--------------Desafio--------------")
 print("")
+print("-----------------------------------")
+print("")
 
 # 1 - A série de Fibonacci é formada pela sequência 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55... Faça um programa que 
 # gere a serie até que o valor seja maior que 500.
